519d62553a76aadb40fba1554979db902921b038
aroy105 <orghois@gmail.com>
2023-06-09 14:54:36 -0400

    Sliding Window templates

---
+++ SlidingWindow/3-longest_substring_without_repeating_characters.py
@@ -0,0 +1,4 @@
+class Solution:
+    """Given a string s, find the length of the longest substring without repeating characters."""
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
