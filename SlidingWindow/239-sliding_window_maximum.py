47abe8aa7ef5e2c008c61fb24ec26d8a9fdbec1d
aroy105 <orghois@gmail.com>
2023-06-13 16:14:38 -0400

    Sliding Window Hard Problems

--- SlidingWindow/239-sliding_window_maximum.py
+++ SlidingWindow/239-sliding_window_maximum.py
@@ -1,3 +1,4 @@
+import collections
 class Solution:
     """
     You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. 
@@ -6,4 +7,35 @@ class Solution:
     Return the max sliding window.
     """
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        pass
+        # The naive solution would be for each sliding window we have, just iterate through it, and add the max value to some kinda list. However, this will be O(k*(n-k))
+        # However, we can optimize this a little. Essentially, if we have a window where there is some x greater than all previous values, we can disregard the previous values
+        # We can use a deque, and in our deque, we'll always have values in decreasing order. As a new higher value comes in, we can pop off all lesser values
+        output = []
+        q = collections.deque() # This will contain indices
+        l = r = 0
+        # While the right side of our sliding window has not reached the end of the array...
+        while r < len(nums):
+            # Remove all values smaller than r in the queue
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            # Add the right most element in our window to the queue
+            q.append(r)
+
+            # OK THIS PART TRIPPED ME UP BECAUSE I WAS STUPID
+            # BASICALLY, l IS OUR CURRENT LEFT WINDOW POINTER
+            # q[0] = THE LEFT MOST INDEX, SINCE Q STORES THE INDICES, NOT THE VALUE IN NUMS
+            # SO IF THE INDEX AT THE FRONT OF OUR DEQUE IS LESS THAN OUR ACTUAL INDEX, POP IT
+            if l > q[0]:
+                q.popleft()
+            
+            # This part is kind of an edge case. Since we initialized l and r to 0, we need to confirm our window is at least size k
+            if (r + 1) >= k:
+                # At each iteration, we need to add the max value to our output. Since we have a monitonic deque, it'll be the value at the front of our queue
+                output.append(nums[q[0]])
+                l += 1 # Once we have a window at least size k, make sure it stays size k by incrementing l each time too.
+            
+            # move the right side of the window too
+            r += 1
+        
+        return output
+                
--- SlidingWindow/76-minimum_window_substring.py
+++ SlidingWindow/76-minimum_window_substring.py
@@ -7,4 +7,56 @@ class Solution:
     The testcases will be generated such that the answer is unique.
     """
     def minWindow(self, s: str, t: str) -> str:
-        pass
+        # The brute force way is to maintain two hashmpas, where called Have and Need. Have is a window which we are currently examining, Need will be fixed. 
+        # As long as all the entries in Have are greater than or equal to the corresponding entries in Need, we have a valid substring. 
+        # However, each time we successively expand our window, we need to double check our previous entries to see if Have[prev] == Need[prev]
+        # But what if we instead kept track of two variables, H and N. Initially, N is set to the length of t, and H is set to 0, but H is incremented everytime an entry in Have is first == entry in Need
+        # When H == N, we know we have found a potential result.
+        # However, this isn't necessarily a minimum result, so we can slide the left side index until we don't have a valid substring. Then we repeat this process. 
+        
+        # Basically, increase the window until we get a valid solution, then decrease until we don't have a valid solution. Repeat until we reach the end of the s. 
+        if t == "": return ""
+        
+        countT, window = {}, {}
+        # Create the table for our Need values. 
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT) # Create our H and N values
+        res, resLen = [-1, -1], float("infinity") # initialize our stored answer
+        # Start the sliding window part
+        l = 0
+        for r in range(len(s)):
+            c = s[r] # the character at the right end of the window
+            window[c] = 1 + window.get(c, 0) # Add this character to our window hashmap
+            
+            # if this character is needed in t, and if the current count of this character is the same in both our window and t...
+            if c in countT and window[c] == countT[c]:
+                have += 1 # One more requirement has been satisfied
+            
+            # while we have a valid substring...
+            while have == need:
+                # if this new substring length is less than our previous min length, store this as the new answer
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    res = r - l + 1
+                
+                # Decrease the size of our valid substring by one
+                window[s[l]] -= 1
+                # If the character we removed was in t and if it wasn't just an excess character (i.e. we needed it for a valid substring)
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    # We now don't have a valid substring, and we must decrease our h counter
+                    have -= 1
+                # move our window from the left one space over
+                l += 1
+        # After our whole sliding window, set our window pointers to the stored optimal solution
+        l, r = res
+        # If we found an optimal solution, return it as a substring of s, and if not, return and empty string. 
+        return s[l:r+1] if resLen != float("infinity") else ""
+        
+                    
+        
+        
+        
+        
+        
