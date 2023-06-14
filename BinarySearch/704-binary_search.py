8df2d65ff54e5780a460b6c66c20bc01129b3234
aroy105 <orghois@gmail.com>
2023-06-14 12:19:24 -0400

    Binary Search templates

---
+++ BinarySearch/704-binary_search.py
@@ -0,0 +1,4 @@
+class Solution:
+    """Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1."""
+    def search(self, nums: List[int], target: int) -> int:
+        pass
