f26ab4cd19f6f58309a3ee2ef8fb245b001d0857
aroy105 <orghois@gmail.com>
2023-06-29 21:19:40 -0400

    Binary Search Easy Problem

--- BinarySearch/704-binary_search.py
+++ BinarySearch/704-binary_search.py
@@ -1,4 +1,18 @@
 class Solution:
-    """Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1."""
+    """
+    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+    If target exists, then return its index. Otherwise, return -1.
+    """
     def search(self, nums: List[int], target: int) -> int:
-        pass
+        l, r = 0, len(nums) - 1
+        
+        # Until l and r converge on a target
+        while l <= r:
+            m = l + ((r-1)//2) # Apparently (l + r) // 2 can lead to overflow
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
