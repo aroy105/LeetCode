eb30c55b35bc6e5ccffab8cdd8fd7e47bb4b514d
aroy105 <orghois@gmail.com>
2023-07-30 23:31:45 -0400

    Tries Medium Problems

--- Tries/208-implement_trie.py
+++ Tries/208-implement_trie.py
@@ -5,14 +5,58 @@ class Trie:
     """
     def __init__(self):
         """Initializes the trie object."""
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
         """Inserts the string word into the trie."""
+        # Set current pointer to the root 
+        curr = self.root
+        # We need to iterate through each char in word, to see if there are existing branches we can build off/if the word exists.
+        for c in word:
+            i = ord(c) - ord("a") # Quickly helps check the index of where the character should be if the word already exists
+            # If our current character is not an existing child, it means we haven't inserted this word yet
+            if curr.children[i] == None:
+                curr.children[i] = TrieNode()
+            # Set the curr pointer to the current letter
+            curr = curr.children[i]
+        # After we have gone through all the children, this means a letter has terminated, so mark it as the end of a word
+        curr.end = True
 
     def search(self, word: str) -> bool:
         """Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise."""
-        
+        ##### POSSIBLE BUG, WHAT IF SEARCH WORD IS LARGER THAN ANY OTHER PREVIOUSLY ENTERED NODE?
+        # Set pointer to top of trie
+        curr = self.root 
+        # Iterate through each letter
+        for c in word:
+            # If current letter is not an existing child, the letter must not be here
+            i = ord(c) - ord("a")
+            if curr.children[i] == None:
+                return False
+            # Update pointer
+            curr = curr.children[i]
+        # We are now at the trie node where our word supposedly ends, so we check the node to see if any previously inserted word ended here.  
+        return curr.end
 
     def startsWith(self, prefix: str) -> bool:
         """Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise."""
+        # Start at top of trie, iterate through each char in prefix
+        curr = self.root 
+        for c in prefix:
+            # If a current character does not exist as a possible child of the previous TrieNode, return false
+            i = ord(c) - ord("a")
+            if curr.children[i] == None:
+                return False 
+            # Update pointer
+            curr = curr.children[i]
+        # By this point, all TrieNodes must have matched up with prefix letters. 
+        return True
         
+
+# NEED TO MAKE A NEW CLASS CALLED A TRIE NODE
+class TrieNode:
+    def __init__(self):
+        # Representing one of 26 possible future children letters
+        self.children = [None]*26
+        # See if this node is an ending
+        self.end = False
--- Tries/211-design_add_and_search_words_data_structure.py
+++ Tries/211-design_add_and_search_words_data_structure.py
@@ -2,15 +2,58 @@ class WordDictionary:
     """Design a data structure that supports adding new words and finding if a string matches any previously added string."""
     def __init__(self):
         """Initializes the object"""
-        
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         """Adds word to the data structure, it can be matched later."""
-        
+        # Set pointer to top of trie
+        cur = self.root 
+        # Iterate through each word
+        for c in word:
+            # If corresponding TrieNode child doesn't exist for word, add it
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            # Update pointer
+            cur = cur.children[c]
+        # Set the current TrieNode's self.word variable to true, to signify the end of a word after iterating through all values
+        cur.word = True
 
     def search(self, word: str) -> bool:
         """
         Returns true if there is any string in the data structure that matches word or false otherwise. 
         word may contain dots '.' where dots can be matched with any letter.
         """
-        
+        # Use dfs to go through all existing words
+        def dfs(j, root):
+            # Set pointer to top of trie
+            cur = root 
+            # Go through each letter in our specified substring, initially set to the whole word. Can be shorter given "."
+            for i in range(j, len(word)):
+                
+                c = word[i]
+                # If we have a placeholder
+                if c == ".":
+                    # We need to go through every possible subtree from here, so recursively run dfs through all the children, starting from the current letter
+                    for child in cur.children.values():
+                        # At the child, run dfs, with i + 1 since it is the next letter in the word
+                        if dfs(i + 1, child):
+                            return True 
+                    # At this point, we must have not found a matching child. 
+                    return False 
+                # If the current character is not "."
+                else:
+                    # If the current character doesn't exist among the children of the previous node, return false. Otherwise, update pointer
+                    if c not in cur.children:
+                        return False 
+                    cur = cur.children[c] 
+            # At this point, we have gone through the length of the entire word. Now we must check if a previously inserted word ended at the current node, so return this truth value. 
+            return cur.word 
+        # Return the solution to dfs at the top of the trie starting from the first word. 
+        return dfs(0, self.root)
+                
+
+# HAVE TO MAKE CLASS FOR EACH OF THE TRIENODES
+class TrieNode:
+    def __init__(self):
+        self.children = {} # individually index new words, like 'a' : TrieNode
+        self.word = False
