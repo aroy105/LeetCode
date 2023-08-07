3c64833ac90a77e678d714ff83b6366a622c8cc9
aroy105 <orghois@gmail.com>
2023-08-07 16:49:37 -0400

    Heap/Priority Queue Easy Problems

--- Heap-PriorityQueue/1046-last_stone_weight.py
+++ Heap-PriorityQueue/1046-last_stone_weight.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution:
     """
     You are given an array of integers stones where stones[i] is the weight of the ith stone.
@@ -9,4 +10,17 @@ class Solution:
     Return the weight of the last remaining stone. If there are no stones left, return 0.
     """
     def lastStoneWeight(self, stones: List[int]) -> int:
-        pass
+        # We need to use a heap to store and simulate the heaviest stones that will be collided
+        # Since python only supports a MinHeap, we can just multiply all numbers by -1 to have it sort by MaxHeap
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            first = abs(heapq.heappop(stones)) # heaviest
+            second = abs(heapq.heappop(stones)) # second heaviest/equal in weight
+            # If second stone will be totally obliterated by first
+            if second < first: 
+                # Return what's left of first back into stones (multiply by -1 since it is a MaxHeap)
+                heapq.heappush(stones, -(first - second))
+                
+        stones.append(0) # A zero weight stone handles an edge case of an empty stones list. This is also the minimum answer for any such list
+        return abs(stones[0])
--- Heap-PriorityQueue/703-kth_largest_element_in_a_stream.py
+++ Heap-PriorityQueue/703-kth_largest_element_in_a_stream.py
@@ -1,14 +1,30 @@
+import heapq
 class KthLargest:
     """
     Design a class to find the kth largest element in a stream. Note that it is the kth largest element in sorted order, not the kth distinct element
     Implement this class
     """
+    # A stream just means we could continue to add elements to the list and evaluate on a rolling basis
+    # We will use a minheap of size k 
     def __init__(self, k: int, nums: List[int]):
         """Initializes the object with the integer k and the stream of integers nums."""
-        pass
+        # Turn our initial data structure into a heap, and set the size of the data structure to be k
+        self.minHeap, self.k = nums, k 
+        # Turn the input data into a heap
+        heapq.heapify(self.minHeap)
+        # Reduce the size of the minheap tuntil it is size k
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+        
+        
     def add(self, val: int) -> int:
         """Appends the integer val to the stream and returns the element representing the kth largest element in the stream."""
-        pass
+        heapq.heappush(self.minHeap, val)
+        # If we exceeded the space of the minHeap (i.e. added an element that must be lower than kth largest element, pop whatever is now the lowest)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        # Return the top of the heap
+        return self.minHeap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
