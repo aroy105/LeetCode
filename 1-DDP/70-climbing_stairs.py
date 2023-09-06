c47c8082fd4c9c88e3955f1cab8f7dae6b589630
aroy105 <orghois@gmail.com>
2023-09-05 23:51:27 -0400

    1-D Dynamic Programming Easy Problems

--- 1-DDP/70-climbing_stairs.py
+++ 1-DDP/70-climbing_stairs.py
@@ -4,4 +4,26 @@ class Solution:
     Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
     """
     def climbStairs(self, n: int) -> int:
-        pass
+        # If steps = 1, only 1 way, if steps = 2, only {1 + 1, 2}, if steps = 3, {1+1+1, 1+2, 2+1}
+        if n <= 3:
+            return n
+        # This will always be the case, where crossing two steps can be done in two ways, and three steps in three ways
+        # This is like our base case, everything else gets built up from this
+        # n1 is like f_n-1, n2 is like f_n
+        n1, n2 = 2, 3
+        
+        # Now, we build up our cases, and consider higher step numbers. For every step higher than our base cases...
+        for i in range(4, n+1):
+            # If we add a step, we can either jump one or two steps forward
+            # Imagine we were at three steps, and now we have to traverse four. We are at Step 4
+            # One hop takes us to step 3, and we know from base cases there are three ways we can get to the end from there
+            # A two-step hop takes us to step 2, and we know from bases cases there are two ways we can get to the end from there
+            # Thus, standing at step 4, there are 5 ways we can get to end. 
+            
+            # However, we need to update our f_n-1 and f_n values for the next step, so push n1 to the current "next step" i.e. n2, and update n2. 
+            # We need a temp variable to accomplish this
+            temp = n1 + n2
+            n1 = n2
+            n2 = temp
+        # Return our current step
+        return n2
--- 1-DDP/746-min_cost_climbing_stairs.py
+++ 1-DDP/746-min_cost_climbing_stairs.py
@@ -5,4 +5,16 @@ class Solution:
     Return the minimum cost to reach the top of the floor.
     """
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        pass
+        # Go backwards, starting from the second to last step to the beginning, and store in each step the cheapest way to the top from that point
+        # From the nth step, we can then just do a 1 jump. From the n-1th step, we can do a two jump. 
+        # We need to start calculating our costs from the n-2th step, since this is the last place where it's impossible to reach the top for free
+        # We then work backwards, and for each step, add the step cost which it's cheapest to jump to and incur
+        # This is a greedy approach
+        for i in range(len(cost) - 3, -1, -1):
+            # In the first iteration, add whichever step cost it's cheapest to jump to and incur, either the n-1 step (1 jump) or n step (2-jump)
+            # iteratively add the accumulated costs down the chain
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        
+        # At this point, every step has the cost of cheapest path from that point. Therefore, steps 1 and 2 have the accumulated costs for going all the way up
+        # Pick which path is cheaper from the two
+        return min(cost[0], cost[1])
