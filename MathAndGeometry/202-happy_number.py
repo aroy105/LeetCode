674d7fea61251aabdc387ef3025de4b5ded877b6
aroy105 <orghois@gmail.com>
2023-10-04 03:52:05 -0400

    Math & Geometry Easy Problems

--- MathAndGeometry/202-happy_number.py
+++ MathAndGeometry/202-happy_number.py
@@ -8,4 +8,28 @@ class Solution:
     Return true if n is a happy number, and false if not.
     """
     def isHappy(self, n: int) -> bool:
-        pass
+        # We can track each number from n to 1 (if possible) as a Linked List
+        # We will use our slow/fast method, and set slow to n, and fast to the node right after n
+        # Since each node one step forward is just the application of the previous node into the sumSquareDigit function
+        slow, fast = n, self.sumSquareDigits(n)
+        
+        # Remember, this method will detect a cycle when slow = fast, since the fast node will cycle ahead and eventually catch up to the slow node from behind
+        while slow != fast:
+            # Increment fast by two jumps (set fast to the next node, and then set that next node to the node after)
+            fast = self.sumSquareDigits(fast)
+            fast = self.sumSquareDigits(fast)
+            # Increment the slow pointer one node forward
+            slow = self.sumSquareDigits(slow)
+        
+        # At this point, the slow and fast pointer have met at a node
+        # If this is a happy number, then fast should be stuck at 1 when slow eventually catches up. If not, slow is at some other value, and fast caught up from behind.
+        return True if fast == 1 else False
+    
+    
+    # The following helper function just adds the sum of each digit of n
+    def sumSquareDigits(self, n):
+        output = 0
+        while n: 
+            output += ((n % 10))**2
+            n = n // 10
+        return output
--- MathAndGeometry/66-plus_one.py
+++ MathAndGeometry/66-plus_one.py
@@ -5,4 +5,26 @@ class Solution:
     Increment the large integer by one and return the resulting array of digits.
     """
     def plusOne(self, digits: List[int]) -> List[int]:
-        pass
+        # This is the "plus one" that we're adding to our integer, and i is the current 'digit' we're looking at
+        one, i = 1, 0
+        # Reverse our digits, so that going from the front to the back of the number means going from the least to most significant digit
+        digits = digits[::-1]
+        # one will also be used to represent 'carry-in' to the current digit. So while we still have a carryover, we need to keep adding stuff
+        while one:
+            # If we haven't gone out of bounds of the array i.e. we don't need a new space in the array
+            if i < len(digits):
+                # If our digit is 9 i.e. 9 + 1 = 10, clear the digit and preseve the carryout. Otherwise, add the carry-in to the current digit, and clear the carry-in
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            # If we now do need to go out of bounds, add the carry-in, and clear the value once added
+            else:
+                digits.append(one)
+                one = 0
+            # Increment our digit pointer, to point to the next digit
+            i += 1
+                
+        # Reverse our digits again
+        return digits[::-1]
